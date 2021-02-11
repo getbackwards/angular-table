1907d6902be3d99baac5cb5a194d98f21aedb8b1
Your Name <getbackwards@gmail.com>
2021-02-12 09:07:48 +1300

    commiting this mess

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+// import {Component} from '@angular/core';
+import { Component, Renderer2, ViewChild, ViewChildren, ElementRef, QueryList } from '@angular/core';
 
 export interface PeriodicElement {
   number: number;
@@ -15,11 +16,71 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {number: 1, date: 'Wed 02/12/20 11:30am', site: 'Putney', with: 'Roy Oven', dur: 30, status: 'Confirmed', claimNo: 1234567, exam: 'Yes', charge: '$0.00', invoiced: '$0.00', paidWo: '$0.00' },
-  {number: 2, date: 'Wed 02/12/20 11:30am', site: 'Putney', with: 'Roy Oven', dur: 30, status: 'Confirmed', claimNo: 1234567, exam: 'Yes', charge: '$0.00', invoiced: '$0.00', paidWo: '$0.00' },
-  {number: 3, date: 'Wed 02/12/20 11:30am', site: 'Putney', with: 'Roy Oven', dur: 30, status: 'Confirmed', claimNo: 1234567, exam: 'Yes', charge: '$0.00', invoiced: '$0.00', paidWo: '$0.00' },
-  {number: 4, date: 'Wed 02/12/20 11:30am', site: 'Putney', with: 'Roy Oven', dur: 30, status: 'Confirmed', claimNo: 1234567, exam: 'Yes', charge: '$0.00', invoiced: '$0.00', paidWo: '$0.00' },
-  {number: 5, date: 'Wed 02/12/20 11:30am', site: 'Putney', with: 'Roy Oven', dur: 30, status: 'Confirmed', claimNo: 1234567, exam: 'Yes', charge: '$0.00', invoiced: '$0.00', paidWo: '$0.00' }
+  {
+    number: 1,
+    date: 'Wed 02/12/20 11:30am',
+    site: 'Putney',
+    with: 'Roy Oven',
+    dur: 30,
+    status: 'Confirmed',
+    claimNo: 1234567,
+    exam: 'Yes',
+    charge: '$0.00',
+    invoiced: '$0.00',
+    paidWo: '$0.00',
+  },
+  {
+    number: 2,
+    date: 'Wed 02/12/20 11:30am',
+    site: 'Putney',
+    with: 'Roy Oven',
+    dur: 30,
+    status: 'Confirmed',
+    claimNo: 1234567,
+    exam: 'Yes',
+    charge: '$0.00',
+    invoiced: '$0.00',
+    paidWo: '$0.00',
+  },
+  {
+    number: 3,
+    date: 'Wed 02/12/20 11:30am',
+    site: 'Putney',
+    with: 'Roy Oven',
+    dur: 30,
+    status: 'Confirmed',
+    claimNo: 1234567,
+    exam: 'Yes',
+    charge: '$0.00',
+    invoiced: '$0.00',
+    paidWo: '$0.00',
+  },
+  {
+    number: 4,
+    date: 'Wed 02/12/20 11:30am',
+    site: 'Putney',
+    with: 'Roy Oven',
+    dur: 30,
+    status: 'Confirmed',
+    claimNo: 1234567,
+    exam: 'Yes',
+    charge: '$0.00',
+    invoiced: '$0.00',
+    paidWo: '$0.00',
+  },
+  {
+    number: 5,
+    date: 'Wed 02/12/20 11:30am',
+    site: 'Putney',
+    with: 'Roy Oven',
+    dur: 30,
+    status: 'Confirmed',
+    claimNo: 1234567,
+    exam: 'Yes',
+    charge: '$0.00',
+    invoiced: '$0.00',
+    paidWo: '$0.00',
+  },
 ];
 
 /**
@@ -31,31 +92,97 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: 'table.component.html',
 })
 export class TableComponent {
-  displayedColumns: string[] = ['number', 'date', 'site', 'with', 'dur', 'status', 'claimNo', 'exam', 'charge', 'invoiced', 'paidWo'];
+  displayedColumns: string[] = [
+    'number',
+    'date',
+    'site',
+    'with',
+    'dur',
+    'status',
+    'claimNo',
+    'exam',
+    'charge',
+    'invoiced',
+    'paidWo',
+  ];
   dataSource = ELEMENT_DATA;
 
-  htmlToAdd;
+  public elements2:Array<unknown> = [];
 
-  td;
-
-  tableRow = document.querySelectorAll('div');
-  // console.log(tableRow);
-
-  mouseEnter(div : string) {
-    console.log("mouse enter : " + div);
+  public appendElement():void {
+    this.elements2 = [...this.elements2, this.elements2.length + 1]; // Just append anything, since you are not using the values otherwise, appending a generic ID here
  }
 
- mouseLeave(div : string){
-   console.log('mouse leave :' + div);
+ public caller(elementId:number):void {
+    console.log('Element', elementId, 'was clicked');
+    // Do whatever you want when your appended elements are clicked
  }
 
- tdEnter(td) {
-  console.log("mouse enter : ");
-  this.htmlToAdd = '<div class="two">two</div>';
-}
+
+  @ViewChild('link') link: ElementRef;
+
+  // @ViewChildren('h') elements:QueryList<ElementRef> 
+
+  // myContainer = document.querySelectorAll("td");
+  myContainer = Array.from(document.querySelectorAll('td'));
+  // myContainer.forEach((e) => console.log(e));
   
-}
+  
 
+  constructor(private renderer: Renderer2) {}
+  
+  // addElement() {
+  //   const a: HTMLParagraphElement = this.renderer.createElement('a');
+  //   a.innerHTML = 'edit';
+  //   a.setAttribute('href', '#');
+  //   a.classList.add('td');
+  //   this.renderer.appendChild(this.link.nativeElement, a);
+  // }
+
+  addElement() {
+    // this.myContainer.forEach((e) => e.parentNode.removeChild(e));
+    // this.myContainer.forEach((e) => e.parentNode.removeChild(e));
+    this.myContainer.forEach((e) => e.innerHTML = 'edit');
+    this.myContainer.forEach((e) => console.log(e));
+    // this.innerHTML = 'edit';
+    // console.log('hello');
+    // console.log(e);
+    console.log(this.myContainer);
+    // const a: HTMLParagraphElement = this.myContainer.createElement('a');
+    // this.myContainer.innerHTML = 'edit';
+
+   this.myContainer.forEach((a) => {
+      console.log(a);
+    });
+
+    console.log(this.myContainer.entries);
+  }
+
+  removeElement() {
+    // const a: HTMLParagraphElement = this.renderer.createElement('a');
+    // a.innerHTML = 'edit';
+    // a.setAttribute('href', '#');
+    // this.renderer.appendChild(this.link.nativeElement, a);
+
+
+    // document.querySelectorAll('.td').forEach((e) => e.parentNode.removeChild(e));
+    // console.log('bye');
+  }
+
+  students = [
+    { id: 1, name: 'Hardik' },
+
+    { id: 2, name: 'Vimal' },
+
+    { id: 3, name: 'Harshad' },
+  ];
+
+  clickEvent(e) {
+    var id = e.getAttribute('data-id');
+
+    console.log(id);
+  }
+}
 
 /**  Copyright 2020 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
