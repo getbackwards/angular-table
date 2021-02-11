c5ae1818baf321bf740f6d7f10f3f904503a59be
Your Name <getbackwards@gmail.com>
2021-02-11 21:57:22 +1300

    added mat table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatSliderModule } from '@angular/material/slider';
+// import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table' 
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { TableBasicExample } from './table/table.component';
+// import { TableComponent } from './table/table.component';
+import { TableBasicExample } from './table/table-basic-example';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableBasicExample
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
